c7760599738980ee72aebecc33537e3c7aa92e8e
iranavsegda <irsensimpson@gmail.com>
2020-11-25 21:57:15 +0200

    slider section

--- gulpfile.js
+++ gulpfile.js
@@ -30,10 +30,10 @@ function styles() {
 	.pipe(browserSync.stream())
 }
 
-
 function scripts() {
 	return src([
 		'node_modules/jquery/dist/jquery.js',
+		'node_modules/slick-carousel/slick/slick.js',
 		'app/js/main.js'
 	])
 	.pipe(concat('main.min.js'))
